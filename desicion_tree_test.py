ed706224435cb8870e10b5ef944b0b6cf6265cac
DruhinMitra <druhinmitra15@gmail.com>
2025-10-23 08:46:20 +0530

    removed extra things

--- desicion_tree_test.py
+++ desicion_tree_test.py
@@ -1,12 +1,2 @@
-import pandas as pd
-import csv
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import accuracy_score
-from sklearn import linear_model
-import matplotlib.pyplot as plt
-import random
 
-df=pd.read_csv('diabetes.csv')
 
